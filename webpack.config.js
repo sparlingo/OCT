b1d9c95e283b51cb79e25ba5ba8f226ccd7baccc
Kevin Sparling <kevinsparling@gmail.com>
2019-08-12 21:57:30 +0800

    working on index, removed pug, added nunjucks

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,41 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-//var GhPagesWebpackPlugin = require('gh-pages-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.html',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'js/bundle.js'
   },
   module: {
-    rules: [{
-      test: /\.scss$/,
-      use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader'
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: true,
-              // options...
+    rules: [
+      {
+        test: /\.scss$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader'
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true,
+                // options...
+              }
             }
-          }
+          ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'         
         ]
-    }]
+      }
+    ]
   },
   plugins: [
     new MiniCssExtractPlugin({
       filename: 'css/styles.css'
     }),
-    // new GhPagesWebpackPlugin({
-    //     path: './public',
-    //     options: {
-    //         message: 'Update sparlingo.github.io',
-    //         user: {
-    //             name: 'Kevin Sparling',
-    //             email: 'kevinsparling@gmail.com'
-    //         }
-    //     }
-    // })
   ]
 };
