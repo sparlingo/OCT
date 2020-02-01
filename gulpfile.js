a8a825ff51cf02f4e8692d33dd26bfd4cade6f1c
Kevin Sparling <kevinsparling@gmail.com>
2020-02-01 22:06:39 +0800

    added nunjucks

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+const gulp = require('gulp');
+const nunjucksRender = require('gulp-nunjucks-render');
+
+gulp.task('nunjucks', function() {
+  // Gets .html and .nunjucks files in pages
+  return gulp.src('pages/**/*.+(njk)')
+  // Renders template with nunjucks
+  .pipe(nunjucksRender({
+      path: ['./templates']
+    }))
+  // output files in app folder
+  .pipe(gulp.dest('./docs'))
+});
